51796ec8e10c7de9de042db6198f5c3972f83e75
rnakai <rnakai@student.42tokyo.jp>
2020-11-13 18:25:25 +0900

    t_game構造体を削除し、関数に説明コメントを加えた

--- srcs/game/definitions.h
+++ srcs/game/definitions.h
@@ -6,7 +6,7 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 10:14:37 by rnakai            #+#    #+#             */
-/*   Updated: 2020/11/13 11:49:05 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/11/13 18:17:52 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include "../../includes/structs/struct_player.h"
 # include "../../includes/structs/struct_ray.h"
 # include "../../includes/structs/struct_rect_line.h"
+#include "../../includes/structs/struct_game.h"
 
 # define TRUE 1
 # define FALSE 0
@@ -63,47 +64,89 @@ extern int		g_key_flag;
 extern t_player	g_player;
 extern t_ray	g_rays[NUM_RAYS];
 
-typedef struct	s_img
-{
-	void	*img;
-	int		*data; //imgの本体
+// typedef struct	s_img
+// {
+// 	void	*img;
+// 	int		*data; //imgの本体
 
-	int		size_l;
-	int		bpp;
-	int		endian;
-}				t_img;
+// 	int		size_l;
+// 	int		bpp;
+// 	int		endian;
+// }				t_img;
 
-typedef struct	s_game
-{
-	void	*mlx;
-	void	*win;
-	t_img	img;
-}				t_game;
+// typedef struct	s_game
+// {
+// 	void	*mlx;
+// 	void	*win;
+// 	t_img	img;
+// }				t_game;
 
+/*
+** my drawing functions
+*/
 
 void			my_mlx_pixel_put(t_game *game, int x, int y, int color);
 void			draw_line
 				(t_game *game, t_line_info line, int color);
 void			draw_player_rect(t_game *game, t_rect_info rect, int color);
 
+
+/*
+** draw_2Dmap utils (from 42seoul git hub)
+*/
+
 void			draw_lines(t_game *game);
 void			draw_rectangle(t_game *game, int x, int y, int color);
 void			draw_rectangles(t_game *game);
 
+
+/*
+** functions: init rect, line struct easily
+*/
+
 t_rect_info		init_rect_info(int x, int y, int width, int height);
 t_line_info		init_line_info(double x1, double y1, double x2, double y2);
+
+
+/*
+** render_functions
+*/
 
 void			render_player(t_game *game);
 void			render_rays(t_game *game);
 void			render_map(t_game *game);
 void			render(t_game *game);
 
+
+/*
+** hook functions
+*/
+
 int				key_pressed(int key_code, t_game *game);
 int				key_released(int key_code, t_game *game);
 int				close(t_game *game);
+void			register_hook(t_game *game);
+
+
+/*
+** utility func
+*/
 
 int				has_wall_at(float x, float y);
+
+
+/*
+** game start functions
+*/
+
+void			initialize_window(t_game *game);
+void			setup(void);
 int				main_loop(t_game *game);
+
+
+/*
+** game update functions
+*/
 
 void			update(void);
 void			cast_rays(void);
