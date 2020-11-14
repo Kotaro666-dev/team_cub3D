24aedffe944358e5af682856e41692bf775da6f2
Kotaro Kamashima <kotaro0726@gmail.com>
2020-11-14 18:16:49 +0900

    Add bunch of error patterns

--- error_test.sh
+++ error_test.sh
@@ -18,11 +18,33 @@ done
 echo "error_pattern/error20.cub"
 ./cub3D error_pattern/error20.cub
 
-for i in `seq 5`
+for i in `seq 9`
 do
 echo " error_pattern/error2$i.cub"
 ./cub3D error_pattern/error2$i.cub
 done
 
-echo " error_pattern/errorformat.cubwrong"
-./cub3D error_pattern/errorformat.cubwrong
+echo "error_pattern/error30.cub"
+./cub3D error_pattern/error30.cub
+
+for i in `seq 7`
+do
+echo " error_pattern/error3$i.cub"
+./cub3D error_pattern/error3$i.cub
+done
+
+echo "error_pattern/error40.cub"
+./cub3D error_pattern/error40.cub
+
+for i in `seq 4`
+do
+echo " error_pattern/error4$i.cub"
+./cub3D error_pattern/error4$i.cub
+done
+
+# WRONG FORMAT
+echo "error_pattern/errorformat01.cubwrong"
+./cub3D error_pattern/errorformat01.cubwrong
+
+echo "error_pattern/errorformat02.cub.cubwrong"
+./cub3D error_pattern/errorformat02.cub.cubwrong
