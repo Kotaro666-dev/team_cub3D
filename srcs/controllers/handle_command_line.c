9301ae820f5c297b72a7b79e0ba01a38f41c591f
Kotaro Kamashima <kotaro0726@gmail.com>
2020-11-14 12:12:19 +0900

    workaround debug: should be refactored much better

--- srcs/controllers/handle_command_line.c
+++ srcs/controllers/handle_command_line.c
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 15:16:21 by kkamashi          #+#    #+#             */
-/*   Updated: 2020/11/14 09:22:05 by kkamashi         ###   ########.fr       */
+/*   Updated: 2020/11/14 12:00:53 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,11 @@ static int	verify_cub_file(char *cub_file, t_game *game)
 	if (!game->cub_data.map_data.have_found_player)
 	{
 		game->err_msg.which_msg = NOT_FOUND_PLAYER;
+		return (ERROR);
+	}
+	if (!is_player_surrounded_by_walls(&game->cub_data))
+	{
+		game->err_msg.which_msg = MAP_NOT_CLOSED;
 		return (ERROR);
 	}
 	return (TRUE);
