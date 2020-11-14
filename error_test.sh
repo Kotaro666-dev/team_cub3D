61db437560666e43f57b4fee6a2879b3b0b2cf1f
Kotaro Kamashima <kotaro0726@gmail.com>
2020-11-14 18:22:37 +0900

    update: dir name changed error_pattern -> error_test

--- error_test.sh
+++ error_test.sh
@@ -2,49 +2,49 @@
 
 for i in `seq 9`
 do
-echo "error_pattern/error0$i.cub"
-./cub3D error_pattern/error0$i.cub
+echo "error_test/error0$i.cub"
+./cub3D error_test/error0$i.cub
 done
 
-echo "error_pattern/error10.cub"
-./cub3D error_pattern/error10.cub
+echo "error_test/error10.cub"
+./cub3D error_test/error10.cub
 
 for i in `seq 9`
 do
-echo "error_pattern/error1$i.cub"
-./cub3D error_pattern/error1$i.cub
+echo "error_test/error1$i.cub"
+./cub3D error_test/error1$i.cub
 done
 
-echo "error_pattern/error20.cub"
-./cub3D error_pattern/error20.cub
+echo "error_test/error20.cub"
+./cub3D error_test/error20.cub
 
 for i in `seq 9`
 do
-echo " error_pattern/error2$i.cub"
-./cub3D error_pattern/error2$i.cub
+echo " error_test/error2$i.cub"
+./cub3D error_test/error2$i.cub
 done
 
-echo "error_pattern/error30.cub"
-./cub3D error_pattern/error30.cub
+echo "error_test/error30.cub"
+./cub3D error_test/error30.cub
 
 for i in `seq 7`
 do
-echo " error_pattern/error3$i.cub"
-./cub3D error_pattern/error3$i.cub
+echo " error_test/error3$i.cub"
+./cub3D error_test/error3$i.cub
 done
 
-echo "error_pattern/error40.cub"
-./cub3D error_pattern/error40.cub
+echo "error_test/error40.cub"
+./cub3D error_test/error40.cub
 
 for i in `seq 4`
 do
-echo " error_pattern/error4$i.cub"
-./cub3D error_pattern/error4$i.cub
+echo " error_test/error4$i.cub"
+./cub3D error_test/error4$i.cub
 done
 
 # WRONG FORMAT
-echo "error_pattern/errorformat01.cubwrong"
-./cub3D error_pattern/errorformat01.cubwrong
+echo "error_test/errorformat01.cubwrong"
+./cub3D error_test/errorformat01.cubwrong
 
-echo "error_pattern/errorformat02.cub.cubwrong"
-./cub3D error_pattern/errorformat02.cub.cubwrong
+echo "error_test/errorformat02.cub.cubwrong"
+./cub3D error_test/errorformat02.cub.cubwrong
