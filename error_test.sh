943e860a1862a0332875249dc01ce2f01700fed5
Kotaro Kamashima <kotaro0726@gmail.com>
2020-11-14 12:11:41 +0900

    Update shell script test

--- error_test.sh
+++ error_test.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-for i in `seq 10`
+for i in `seq 9`
 do
 echo "error_pattern/error0$i.cub"
 ./cub3D error_pattern/error0$i.cub
 done
 
-for i in `seq 10`
+echo "error_pattern/error10.cub"
+./cub3D error_pattern/error10.cub
+
+for i in `seq 9`
 do
 echo "error_pattern/error1$i.cub"
 ./cub3D error_pattern/error1$i.cub
 done
+
+echo "error_pattern/error20.cub"
+./cub3D error_pattern/error20.cub
 
 for i in `seq 5`
 do
