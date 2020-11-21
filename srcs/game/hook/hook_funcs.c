a0612cf7719e2f128f39ddede969d9244e53eb30
rnakai <rnakai@student.42tokyo.jp>
2020-11-21 15:44:07 +0900

    mlx関係の不具合を調整した

--- srcs/game/hook/hook_funcs.c
+++ srcs/game/hook/hook_funcs.c
@@ -6,7 +6,7 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 11:44:19 by rnakai            #+#    #+#             */
-/*   Updated: 2020/11/20 15:18:56 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/11/21 15:34:55 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 #include "key_xevent_code.h"
 #include "game.h"
 #include <stdlib.h>
+#include "mlx.h"
 
 int		key_pressed(int key_code, t_game *game)
 {
 	(void)game;
 	if (key_code == KEY_ESC)
-		exit(0);
+		close_window(game);
 	else if (key_code == KEY_W)
 		g_player.walk_direction = 1;
 	else if (key_code == KEY_S)
@@ -71,7 +72,10 @@ int		key_released(int key_code, t_game *game)
 int		close_window(t_game *game)
 {
 	free(g_rays);
+	mlx_destroy_image(game->mlx, game->image.img);
+	mlx_destroy_image(game->mlx, g_textures[0].img_ptr);
+	mlx_destroy_window(game->mlx, game->win);
+	mlx_destroy_display(game->mlx);
 	exit(0);
-	(void)game;
 	return (0);
 }
