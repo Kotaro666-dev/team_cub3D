c42ba26b76e6bb713d3535f54574835be3689229
rnakai <rnakai@student.42tokyo.jp>
2020-11-24 15:16:12 +0900

    spriteに進入できないようにした

--- srcs/game/system/update.c
+++ srcs/game/system/update.c
@@ -6,7 +6,7 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 12:40:01 by rnakai            #+#    #+#             */
-/*   Updated: 2020/11/20 15:32:31 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/11/24 15:14:38 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,8 @@ void	move_player(void)
 	move_step = g_player.walk_direction * g_player.walk_speed;
 	new_player_x = g_player.x + move_step * cos(g_player.rotation_angle + g_player.side_angle);
 	new_player_y = g_player.y + move_step * sin(g_player.rotation_angle + g_player.side_angle);
-	if (!has_wall_at(new_player_x, new_player_y))
+	if (!has_wall_at(new_player_x, new_player_y) &&
+		!has_sprite_at(new_player_x, new_player_y))
 	{
 		g_player.x = new_player_x;
 		g_player.y = new_player_y;
