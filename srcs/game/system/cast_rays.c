4468baef923af6b3c890fb87358c27a2c2607a0a
rnakai <rnakai@student.42tokyo.jp>
2020-11-27 15:28:40 +0900

    update.cを削除し、updateはmainloopに入れ、moveplayerとcast_raysを分離した

--- srcs/game/system/cast_rays.c
+++ srcs/game/system/cast_rays.c
@@ -1,32 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_loop.c                                        :+:      :+:    :+:   */
+/*   cast_rays.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/24 14:41:09 by rnakai            #+#    #+#             */
-/*   Updated: 2020/11/27 15:25:41 by rnakai           ###   ########.fr       */
+/*   Created: 2020/11/27 15:23:22 by rnakai            #+#    #+#             */
+/*   Updated: 2020/11/27 15:24:15 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cast_ray.h"
 #include "game.h"
 
-int		main_loop(t_game *game)
+void	cast_rays(void)
 {
-	if (g_info.key_flag == TRUE)
-	{
-		//	描画する
-		update();
-		render(game);
-	}
-	g_info.key_flag = FALSE;
-	return (0);
-}
+	int		strip_id;
+	float	ray_angle;
 
-void	update(void)
-{
-	move_player();
-	cast_rays();
-	//init_sprite_data();
+	strip_id = 0;
+	ray_angle = g_player.rotation_angle - (FOV_ANGLE / 2);
+	while (strip_id < g_info.num_rays)
+	{
+		cast_ray(ray_angle, strip_id);
+		ray_angle += FOV_ANGLE / g_info.num_rays;
+		strip_id++;
+	}
 }
--- srcs/game/system/move_player.c
+++ srcs/game/system/move_player.c
@@ -1,41 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   update.c                                           :+:      :+:    :+:   */
+/*   move_player.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 12:40:01 by rnakai            #+#    #+#             */
-/*   Updated: 2020/11/27 12:30:27 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/11/27 15:26:39 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cast_ray.h"
-#include "constants.h"
 #include "game.h"
 #include <math.h>
-
-void	update(void)
-{
-	move_player();
-	cast_rays();
-	//init_sprite_data();
-}
-
-void	cast_rays(void)
-{
-	int		strip_id;
-	float	ray_angle;
-
-	strip_id = 0;
-	ray_angle = g_player.rotation_angle - (FOV_ANGLE / 2);
-	while (strip_id < g_info.num_rays)
-	{
-		cast_ray(ray_angle, strip_id);
-		ray_angle += FOV_ANGLE / g_info.num_rays;
-		strip_id++;
-	}
-}
 
 void	move_player(void)
 {
