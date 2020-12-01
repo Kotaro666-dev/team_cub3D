f1f57a04c5f4dffde08eb5fd06dd924f15ccee99
rnakai <rnakai@student.42tokyo.jp>
2020-12-01 20:09:13 +0900

    get_element_by_id関数を実装

--- srcs/game/linked_list/get_element.c
+++ srcs/game/linked_list/get_element.c
@@ -6,7 +6,7 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 11:20:58 by rnakai            #+#    #+#             */
-/*   Updated: 2020/12/01 11:23:15 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/12/01 13:18:21 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,16 @@
 
 t_sprite_data	*get_element_by_id(int map_id)
 {
-	;
+	t_sprite_list	*current;
+
+	current = g_nil->next;
+	while (current != g_nil)
+	{
+		if (current->data.map_id == map_id)
+			return (&current->data);
+		current = current->next;
+	}
+	return (NULL);
 }
 
 t_sprite_list	*get_first_element(void)
