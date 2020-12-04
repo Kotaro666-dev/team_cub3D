ed036739c12b6e2524723ff59326c6473fd01640
rnakai <rnakai@student.42tokyo.jp>
2020-12-04 16:48:41 +0900

    setup空白行削除

--- srcs/game/system/setup.c
+++ srcs/game/system/setup.c
@@ -6,7 +6,7 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 16:00:11 by rnakai            #+#    #+#             */
-/*   Updated: 2020/12/04 12:52:29 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/12/04 16:37:17 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,19 @@ void	setup(t_game *game)
 		game->cub_elems.map_data.player_pos_y * TILE_SIZE + TILE_SIZE / 2;
 	g_player.width = 5;
 	g_player.height = 5;
-	g_player.turn_direction = 0; //-1 if left, +1 if right
-	g_player.walk_direction = 0; //-1 if back, +1 if front
+	g_player.turn_direction = 0;
+	g_player.walk_direction = 0;
 	g_player.side_angle = 0;
 	g_player.rotation_angle = convert_orient_ch2angle(
 		game->cub_elems.map_data.player_orient);
 	g_player.walk_speed = 2;
 	g_player.turn_speed = 5 * (PI / 180);
-
 	convert_ch2num_map(g_map, game->cub_elems.map_data.map);
-
 	g_sprite_constructor();
-
 	g_info.key_flag = TRUE;
 	g_info.width = game->cub_elems.rez.width;
 	g_info.height = game->cub_elems.rez.height;
 	g_info.num_rays = g_info.width;
-
 	g_rays = (t_ray*)malloc(sizeof(t_ray) * (game->cub_elems.rez.width + 1));
-
 	store_textures(game);
 }
