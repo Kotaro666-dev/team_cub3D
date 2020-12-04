84c2ad99d56a427f90c90baccc1752045192105f
rnakai <rnakai@student.42tokyo.jp>
2020-12-04 12:10:05 +0900

    render3dwalls関数の分割

--- srcs/game/render/render_3d_walls.c
+++ srcs/game/render/render_3d_walls.c
@@ -6,7 +6,7 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 21:42:15 by rnakai            #+#    #+#             */
-/*   Updated: 2020/11/29 10:12:54 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/12/04 12:08:29 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,7 @@ static void		set_3d_wall_info(t_3d_prj *pj, int i)
 	pj->distance_prj_plane = (g_info.width / 2) / tan(FOV_ANGLE / 2);
 	pj->prjctd_wall_height = (TILE_SIZE / pj->perp_distance) *
 		pj->distance_prj_plane;
-	//
 	pj->wall_strip_height = (int)pj->prjctd_wall_height;
-	//
 	pj->wall_top_pixel = (g_info.height / 2) - (pj->wall_strip_height / 2);
 	pj->wall_top_pixel = (pj->wall_top_pixel < 0)
 		? 0 : pj->wall_top_pixel;
@@ -37,14 +35,14 @@ static void		set_3d_wall_info(t_3d_prj *pj, int i)
 
 static void		set_texture_offset_x(t_3d_prj *pj, int i, int tex_idx)
 {
-		if (g_rays[i].was_hit_vertical)
-			pj->tex_offset_x = (int)g_rays[i].wall_hit_y % TILE_SIZE;
-		else
-			pj->tex_offset_x = (int)g_rays[i].wall_hit_x % TILE_SIZE;
-		if ((g_rays[i].is_ray_facing_left && g_rays[i].was_hit_vertical) ||
-			(g_rays[i].is_ray_facing_down && !g_rays[i].was_hit_vertical))
-			pj->tex_offset_x = TILE_SIZE - pj->tex_offset_x;
-		pj->tex_offset_x *= g_textures[tex_idx].width / TILE_SIZE;
+	if (g_rays[i].was_hit_vertical)
+		pj->tex_offset_x = (int)g_rays[i].wall_hit_y % TILE_SIZE;
+	else
+		pj->tex_offset_x = (int)g_rays[i].wall_hit_x % TILE_SIZE;
+	if ((g_rays[i].is_ray_facing_left && g_rays[i].was_hit_vertical) ||
+		(g_rays[i].is_ray_facing_down && !g_rays[i].was_hit_vertical))
+		pj->tex_offset_x = TILE_SIZE - pj->tex_offset_x;
+	pj->tex_offset_x *= g_textures[tex_idx].width / TILE_SIZE;
 }
 
 static int		get_texture_orient_index(int i)
@@ -57,6 +55,14 @@ static int		get_texture_orient_index(int i)
 		return (EAST_IDX);
 	else
 		return (WEST_IDX);
+}
+
+static void		set_texture_offset_y(t_3d_prj *pj, int j, int tex_idx)
+{
+	pj->distance_from_top =
+		j + (pj->wall_strip_height / 2) - (g_info.height / 2);
+	pj->tex_offset_y = pj->distance_from_top *
+		((float)g_textures[tex_idx].height / pj->wall_strip_height);
 }
 
 void			render_3d_walls(t_game *game)
@@ -72,17 +78,12 @@ void			render_3d_walls(t_game *game)
 		tex_idx = get_texture_orient_index(i);
 		set_3d_wall_info(&pj, i);
 		set_texture_offset_x(&pj, i, tex_idx);
-
 		j = pj.wall_top_pixel;
 		while (j < pj.wall_bottom_pixel)
 		{
-			pj.distance_from_top =
-				j + (pj.wall_strip_height / 2) - (g_info.height / 2);
-			pj.tex_offset_y = pj.distance_from_top *
-				((float)g_textures[tex_idx].height / pj.wall_strip_height);
+			set_texture_offset_y(&pj, j, tex_idx);
 			pj.texel_color =	
 				get_texel_color(pj.tex_offset_x, pj.tex_offset_y, tex_idx);
-
 			my_mlx_pixel_put(game, i, j, pj.texel_color);
 			j++;
 		}
