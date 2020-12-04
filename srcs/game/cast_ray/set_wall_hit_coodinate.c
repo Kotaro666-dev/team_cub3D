d8f39b75e5ea7ec6e43e5ac4a9824c7879949bc0
rnakai <rnakai@student.42tokyo.jp>
2020-12-04 17:05:53 +0900

    set_first_intercept関数内でifブロックを書くことで可読性を挙げた

--- srcs/game/cast_ray/set_wall_hit_coodinate.c
+++ srcs/game/cast_ray/set_wall_hit_coodinate.c
@@ -6,7 +6,7 @@
 /*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 22:51:57 by rnakai            #+#    #+#             */
-/*   Updated: 2020/12/04 16:52:48 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/12/04 17:03:44 by rnakai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,22 @@ void		hrz_set_first_intercept(t_cast_ray_var *hrz,
 	hrz->found_wall_hit = FALSE;
 	hrz->yintercept = floor(g_player.y / TILE_SIZE) * TILE_SIZE;
 	if (cmn->is_ray_facing_down)
+	{
 		hrz->yintercept += TILE_SIZE;
+	}
 	hrz->xintercept =
 		g_player.x + (hrz->yintercept - g_player.y) / tan(cmn->ray_angle);
 	hrz->ystep = TILE_SIZE;
 	if (cmn->is_ray_facing_up)
+	{
 		hrz->ystep *= -1;
+	}
 	hrz->xstep = TILE_SIZE / tan(cmn->ray_angle);
 	if ((cmn->is_ray_facing_left && hrz->xstep > 0) ||
 		(cmn->is_ray_facing_right && hrz->xstep < 0))
+	{
 		hrz->xstep *= -1;
+	}
 	hrz->next_touch_x = hrz->xintercept;
 	hrz->next_touch_y = hrz->yintercept;
 }
@@ -51,7 +57,7 @@ void		hrz_cast_ray_until_wall(t_cast_ray_var *hrz,
 		&& hrz->next_touch_y >= 0 && hrz->next_touch_y <= MAP_HEIGHT)
 	{
 		if (g_info.which_mode == DETECTING_SPRITE &&
-			validate_sprite_hit(hrz, cmn, HORZ) == TRUE)
+					validate_sprite_hit(hrz, cmn, HORZ) == TRUE)
 			set_sprite_data(cmn, hrz);
 		hrz->x_to_check = hrz->next_touch_x;
 		hrz->y_to_check = hrz->next_touch_y + (cmn->is_ray_facing_up ? -1 : 0);
@@ -82,26 +88,24 @@ void		vrt_set_first_intercept(t_cast_ray_var *vrt,
 				t_cast_ray_var_common *cmn)
 {
 	vrt->found_wall_hit = FALSE;
-	//
-	// Find the y-coordinate of the closest horizontal grid intersection
 	vrt->xintercept = floor(g_player.x / TILE_SIZE) * TILE_SIZE;
 	if (cmn->is_ray_facing_right)
+	{
 		vrt->xintercept += TILE_SIZE;
-	//
-	// Find the x-coordinate of the closest horizontal grid intersection
+	}
 	vrt->yintercept =
 		g_player.y + (vrt->xintercept - g_player.x) * tan(cmn->ray_angle);
-	//
-	// Calculate the increment xstep and ystep
 	vrt->xstep = TILE_SIZE;
 	if (cmn->is_ray_facing_left)
+	{
 		vrt->xstep *= -1;
-	//
+	}
 	vrt->ystep = TILE_SIZE * tan(cmn->ray_angle);
 	if ((cmn->is_ray_facing_up && vrt->ystep > 0) ||
 		(cmn->is_ray_facing_down && vrt->ystep < 0))
+	{
 		vrt->ystep *= -1;
-	//
+	}
 	vrt->next_touch_x = vrt->xintercept;
 	vrt->next_touch_y = vrt->yintercept;
 }
@@ -117,7 +121,7 @@ void		vrt_cast_ray_until_wall(t_cast_ray_var *vrt,
 		&& vrt->next_touch_y >= 0 && vrt->next_touch_y <= MAP_HEIGHT)
 	{
 		if (g_info.which_mode == DETECTING_SPRITE &&
-			validate_sprite_hit(vrt, cmn, VERT) == TRUE)
+					validate_sprite_hit(vrt, cmn, VERT) == TRUE)
 			set_sprite_data(cmn, vrt);
 		vrt->x_to_check = vrt->next_touch_x +
 					(cmn->is_ray_facing_left ? -1 : 0);
