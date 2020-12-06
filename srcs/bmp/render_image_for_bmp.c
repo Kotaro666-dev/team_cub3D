e6f1b7d7770d0ef74679882124a8269156320f59
Kotaro666-dev <kotaro0726@gmail.com>
2020-12-06 22:31:04 +0900

    Add funcs to render sprite and refactor as well.

--- srcs/bmp/render_image_for_bmp.c
+++ srcs/bmp/render_image_for_bmp.c
@@ -3,24 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   render_image_for_bmp.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rnakai <rnakai@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/22 17:14:44 by rnakai            #+#    #+#             */
-/*   Updated: 2020/11/27 15:38:20 by rnakai           ###   ########.fr       */
+/*   Updated: 2020/12/06 22:29:11 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 #include "mlx.h"
 
-void		render_image_for_bmp(t_game *game)
+static void	setup_for_bmp(t_game *game)
 {
 	setup(game);
 	game->image.img = mlx_new_image(game->mlx, g_info.width, g_info.height);
 	game->image.buffer =
 		(int *)mlx_get_data_addr(game->image.img, &game->image.bpp,
 								&game->image.size_line, &game->image.endian);
+}
+
+static void	cast_rays_for_bmp(void)
+{
+	g_info.which_mode = DETECTING_WALLS;
 	cast_all_rays();
+	g_info.which_mode = DETECTING_SPRITE;
+	g_sprite.init();
+	cast_all_rays();
+	g_sprite.sort();
+}
+
+static void	render_objects_for_bmp(t_game *game)
+{
 	render_background(game);
 	render_3d_walls(game);
+	if (g_sprite.get_1st_elem() != g_sprite.get_nil())
+	{
+		render_all_sprites(game);
+	}
+}
+
+void		render_image_for_bmp(t_game *game)
+{
+	setup_for_bmp(game);
+	cast_rays_for_bmp();
+	render_objects_for_bmp(game);
 }
